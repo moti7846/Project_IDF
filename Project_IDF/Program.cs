08a0b29d01b9b9fe73a07936e12eceed1c43f1c7
moti7846 <8563816@gmail.com>
2025-05-22 16:29:37 +0300

    first building Hamas

--- Project_IDF/Program.cs
+++ Project_IDF/Program.cs
@@ -10,6 +10,20 @@ namespace Project_IDF
     {
         static void Main(string[] args)
         {
+            Hamas a = new Hamas();
+            a.set_Trorist("mm", 5);
+            a.set_Trorist("yy", 2);
+            //a.get_Trorist();
+            a.Set_weapaon("mm", "m16");
+            a.Set_weapaon("yy", "568a");
+            a.Set_weapaon("mm", "m12");
+            a.Set_weapaon("yy", "yoyo");
+            a.Set_weapaon("mm", "f6");
+            a.Set_weapaon("yy", "vbx");
+            a.get_weapaon("mm");
+            a.get_weapaon("yy");
+
+
         }
     }
 }

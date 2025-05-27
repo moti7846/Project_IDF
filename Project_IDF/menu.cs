79949f20daa49bd2f6d2b3e1835b9640f2edd99b
moti7846 <8563816@gmail.com>
2025-05-27 17:54:42 +0300

    first init menu

--- Project_IDF/menu.cs
+++ Project_IDF/menu.cs
@@ -30,68 +30,240 @@ namespace Project_IDF
             return choice;
         }
 
-        public static void start()
+        public void start()
         {
-
+            PrintMenu();
+            menu();
         }
 
         public void menu()
         {
             int choice = ChoiceMenu();
-            
             switch (choice)
             {
                 case 1:
-                    //מלאי - מעבר לתפריט  ל1_1
-
+                    //תקיפה - מעבר לתפריט
+                    PrintMenu_1();
+                    menu_1();
                     break;
                 case 2:
-                    // תקיפה - מעבר לתפריט ל1_2
-                    break;
-                case 3:
-                    // אמ"ן - מעבר לתפריט ל1_2
+                    // אמן - מעבר לתפריט ל1_2
+                    PrintMenu_2();
+                    menu_2();
                     break;
                 case 10:
-                    //exit
+                    //יציאה מהתוכנית
+                    Console.WriteLine("good day !");
                     return;
+                default:
+                    Console.WriteLine("choice: 1 or 2 or 10.");
+                    menu();
+                    break;
             }
         }
         public void PrintMenu()
         {
-            Console.WriteLine("1. התקפה)");
-            Console.WriteLine("2. אמן");
+            Console.WriteLine("1. Attack)");
+            Console.WriteLine("2. Artist");
+            Console.WriteLine("10. Exit");
 
         }
-
         public void menu_1()
+        {
+            int choice = ChoiceMenu();
+                switch (choice)
+                {
+                    case 1:
+                        //תקיפה - מעבר לתפריט
+                        PrintMenu_1_1();
+                        menu_1_1();
+                        break;
+                    case 2:
+                        // אמן - מעבר לתפריט ל1_2
+                        PrintMenu_1_2();
+                        menu_1_2();
+                        break;
+                    case 9:
+                        // חזרה תפריט אחד קודם
+                        PrintMenu();
+                        menu();
+                        break;
+                    case 10:
+                        //יציאה מהתוכנית
+                        Console.WriteLine("good day !");
+                        return;
+                    default:
+                        Console.WriteLine("choice: 1 or 2 or 3 or 9 or 10.");
+                        menu_1();
+                        break;
+                }
+            start();
+        }
+        public void PrintMenu_1()
+        {
+            Console.WriteLine("1. Inventory");
+            Console.WriteLine("2. Attack");
+            Console.WriteLine("3. History Attack");
+            Console.WriteLine("9. Back");
+            Console.WriteLine("10. Exit");
+        }
+        public void menu_1_1()
         {
             int choice = ChoiceMenu();
             switch (choice)
             {
                 case 1:
-                    //תקיפה - מעבר לתפריט  ל1_1
+                    //תקיפה - מעבר לתפריט
 
                     break;
                 case 2:
-                    // אמ"ן - מעבר לתפריט ל1_2
+                    // אמן - מעבר לתפריט ל1_2
 
                     break;
+                case 9:
+                    // חזרה תפריט אחד קודם
+                    PrintMenu_1();
+                    menu_1();
+                    break;
                 case 10:
-                    //exit
+                    //יציאה מהתוכנית
+                    Console.WriteLine("good day !");
                     return;
+                default:
+                    Console.WriteLine("choice: 1 or 2 or 9 or 10.");
+                    menu_1_1();
+                    break;
             }
+            start();
         }
+        public void PrintMenu_1_1()
+        {
+            Console.WriteLine("1. View Inventory");
+            Console.WriteLine("2. Update Inventory");
+            Console.WriteLine("9. Back");
+            Console.WriteLine("10. Exit");
 
+        }
+        public void menu_1_2()
+        {
+            int choice = ChoiceMenu();
+            switch (choice)
+            {
+                case 1:
+                    //תקיפה - מעבר לתפריט
+
+                    break;
+                case 2:
+                    // אמן - מעבר לתפריט ל1_2
+
+                    break;
+                case 9:
+                    // חזרה תפריט אחד קודם
+                    PrintMenu_1();
+                    menu_1();
+                    break;
+                case 10:
+                    //יציאה מהתוכנית
+                    Console.WriteLine("good day !");
+                    return;
+                default:
+                    Console.WriteLine("choice: 1 or 2 or 9 or 10.");
+                    menu_1_2();
+                    break;
+            }
+            start();
+        }
+        public void PrintMenu_1_2()
+        {
+            Console.WriteLine("1. Automatic attack");
+            Console.WriteLine("2. Manual attack");
+            Console.WriteLine("9. Back");
+            Console.WriteLine("10. Exit");
+
+        }
+        public void menu_1_3()
+        {
+            int choice = ChoiceMenu();
+            switch (choice)
+            {
+                case 1:
+                    //תקיפה - מעבר לתפריט
+
+                    break;
+                case 2:
+                    // אמן - מעבר לתפריט ל1_2
+
+                    break;
+                case 9:
+                    // חזרה תפריט אחד קודם
+                    PrintMenu_1();
+                    menu_1();
+                    break;
+                case 10:
+                    //יציאה מהתוכנית
+                    Console.WriteLine("good day !");
+                    return;
+                default:
+                    Console.WriteLine("choice:9 or 10.");
+                    menu_1_3();
+                    break;
+            }
+            start();
+        }
+        public void PrintMenu_1_3()
+        {
+            Console.WriteLine("1. null");
+            Console.WriteLine("2. null");
+            Console.WriteLine("9. Back");
+            Console.WriteLine("10. Exit");
+
+        }
+        ///
         public void menu_2()
         {
-
+            int choice = ChoiceMenu();
+            switch (choice)
+            {
+                case 1:
+                    //תקיפה - מעבר לתפריט
+                    Console.WriteLine("null");
+                    break;
+                case 2:
+                    // אמן - מעבר לתפריט ל1_2
+                    Console.WriteLine("null");
+                    break;
+                case 3:
+                    //תקיפה - מעבר לתפריט
+                    Console.WriteLine("null");
+                    break;
+                case 4:
+                    // אמן - מעבר לתפריט ל1_2
+                    Console.WriteLine("null");
+                    break;
+                case 9:
+                    // חזרה תפריט אחד קודם
+                    PrintMenu();
+                    menu();
+                    break;
+                case 10:
+                    //יציאה מהתוכנית
+                    Console.WriteLine("good day !");
+                    return;
+                default:
+                    Console.WriteLine("choice: 1 or 2 or 3 or 4 or 9 or 10.");
+                    menu_2();
+                    break;
+            }
+            start();
         }
         public void PrintMenu_2()
         {
-
-            Console.WriteLine("1. Input a Series. (Replace the current series)");
-            Console.WriteLine("2. Display the series in the order it was entered.");
+            Console.WriteLine("1. Add a terrorist");
+            Console.WriteLine("2. Update a terrorist");
+            Console.WriteLine("3. Show a terrorist");
+            Console.WriteLine("4. Show terrorists");
+            Console.WriteLine("9. Back");
+            Console.WriteLine("10. Exit");
         }
-        
     }
 }
